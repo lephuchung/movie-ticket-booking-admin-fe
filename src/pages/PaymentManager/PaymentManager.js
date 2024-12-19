58797ba1aab246452421be1321d74c36c703a320
Hùng Phi <hung.np194581@sis.hust.edu.vn>
2024-12-19 20:34:03 +0700

    paymentmanagerbase

---
+++ src/pages/PaymentManager/PaymentManager.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './PaymentManager.scss';
+
+const PaymentManager = () => {
+
+  return (
+    <div className="page-container">
+      <h1 className="page-title">Quản lý thanh toán</h1>
+      
+    </div>
+  );
+};
+
+export default PaymentManager;
